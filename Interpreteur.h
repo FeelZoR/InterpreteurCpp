0b3b052095140ac151e835fff6d2ce6e13af6e82
KRATTINGER Nils <krattinn@hostname.iut2.upmf-grenoble.fr>
2019-10-09 15:49:02 +0200

    Debut de repeter

--- Interpreteur.h
+++ Interpreteur.h
@@ -27,13 +27,14 @@ private:
 
     // Implémentation de la grammaire
     Noeud*  programme();   //   <programme> ::= procedure principale() <seqInst> finproc FIN_FICHIER
-    Noeud*  seqInst();	   //     <seqInst> ::= <inst> { <inst> }
-    Noeud*  inst();	       //        <inst> ::= <affectation> ; | <instSi>
-    Noeud*  affectation(); // <affectation> ::= <variable> = <expression> 
-    Noeud*  expression();  //  <expression> ::= <facteur> { <opBinaire> <facteur> }
-    Noeud*  facteur();     //     <facteur> ::= <entier>  |  <variable>  |  - <facteur>  | non <facteur> | ( <expression> )
+    Noeud*  seqInst();	   //   <seqInst> ::= <inst> { <inst> }
+    Noeud*  inst();	   //   <inst> ::= <affectation> ; | <instSi>
+    Noeud*  affectation(); //   <affectation> ::= <variable> = <expression> 
+    Noeud*  expression();  //   <expression> ::= <facteur> { <opBinaire> <facteur> }
+    Noeud*  facteur();     //   <facteur> ::= <entier>  |  <variable>  |  - <facteur>  | non <facteur> | ( <expression> )
                            //   <opBinaire> ::= + | - | *  | / | < | > | <= | >= | == | != | et | ou
-    Noeud*  instSi();      //      <instSi> ::= si ( <expression> ) <seqInst> finsi
+    Noeud*  instSi();      //   <instSi> ::= si ( <expression> ) <seqInst> finsi
+    Noeud*  repeter();     //   <instRepeter> ::=repeter <seqInst> jusqua( <expression> )
 
     // outils pour simplifier l'analyse syntaxique
     void tester (const string & symboleAttendu) const;   // Si symbole courant != symboleAttendu, on lève une exception
